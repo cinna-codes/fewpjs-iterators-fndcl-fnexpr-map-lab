05a319c6cdaab3ae246357e208d8a634fba087ab
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-23 14:42:55 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+const titleCased = tutorials.map((sentence) => {
+  const words = sentence.split(" ") // creates an array of all the individual words
+  return words.map((word) => { // iterate over the array of individual words & return a new array
+    return word[0].toUpperCase() + word.substring(1) // `word[0]` is the first letter of the word; `word.substring(1)` returns the second character of the string + everything after it. `.substring()` can also take two arguments, eg `"Test".substring(1,3)` would return "es"
+  }).join(" ") // and join the word array back together into a single string
+})
+
+// Original
+// const titleCased = () => {
+//   return tutorials
+// }
